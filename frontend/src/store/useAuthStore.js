e807291fea27a16d08177d9575a77826ee8d874c
dagisolo10 <dagisolo1023l@gmail.com>
2026-01-07 18:32:17 +0300

    test

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import api from "../config/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : import.meta.env.VITE_API_URL;
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
