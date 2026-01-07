ed2d971810e21f7eee531d23c5cc69c99bfdbe7e
dagisolo10 <dagisolo1023l@gmail.com>
2026-01-07 21:18:07 +0300

    responsive

--- frontend/src/components/ChatAnimatedBorder.jsx
+++ frontend/src/components/ChatAnimatedBorder.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ChatAnimatedBorder({ children }) {
     return (
-        <div className="h-[90vh] relative overflow-hidden w-full z-1 flex items-center justify-center">
+        <div className="h-screen relative overflow-hidden w-full z-1 flex items-center justify-center">
             <div className="z-1 animated-border w-full h-full p-0.25 rounded-2xl">
                 <div className="flex z-3 bg-main w-full h-full border border-white/10 rounded-2xl">{children}</div>
             </div>
